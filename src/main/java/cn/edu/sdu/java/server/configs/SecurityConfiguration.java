4014b118e01694fdc864b775d60981e34941bbfd
Lv117 <2829322168@qq.com>
2026-03-19 18:44:37 +0800

    修复跨域配置

--- src/main/java/cn/edu/sdu/java/server/configs/SecurityConfiguration.java
+++ src/main/java/cn/edu/sdu/java/server/configs/SecurityConfiguration.java
@@ -83,9 +83,9 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8005"));
-        configuration.setAllowedMethods(List.of("GET","POST"));
-        configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
